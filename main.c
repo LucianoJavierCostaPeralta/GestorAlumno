59bfc018575ed788e68e5834853c95624e1262ea
luciano <lucianocostaperalta@gmail.com>
2025-06-22 17:58:08 -0300

    Add function to calculate student average grade

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h> // Agrega esta línea
 
 #define MAX_ALUMNOS 100
 #define MAX_NOMBRE 50
@@ -12,8 +13,28 @@ typedef struct
     int cantidadNotas;
 } Alumno;
 
-int main()
+void cargarAlumno(Alumno *alumno)
 {
+    printf("Nombre: ");
+    fgets(alumno->nombre, MAX_NOMBRE, stdin);
+    alumno->nombre[strcspn(alumno->nombre, "\n")] = 0;
 
+    printf("DNI: ");
+    scanf("%d", &alumno->dni);
+
+    printf("Cantidad de notas: ");
+    scanf("%d", &alumno->cantidadNotas);
+
+    for (int i = 0; i < alumno->cantidadNotas; i++)
+    {
+        printf("Nota %d: ", i + 1);
+        scanf("%f", &alumno->notas[i]);
+    }
+    getchar(); // limpiar buffer
+}
+
+int main(int argc, char const *argv[])
+{
+    printf("Bienvenido al sistema de gestion de alumnos.\n");
     return 0;
 }
